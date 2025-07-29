eb8ea36ccc634daddb5e21ce4d4b87af517c4ee3
sreerajanp <33514791+sreerajanp@users.noreply.github.com>
2025-07-29 20:14:09 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+    build: {
+    outDir: 'dist',
+  },
+  base: '/',
   plugins: [react()],
 })
