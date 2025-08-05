1be93dbecea8bc74ab348aa272c6186e04b166a3
sreerajanp <33514791+sreerajanp@users.noreply.github.com>
2025-08-05 21:19:22 +0200

    Feature/fixes related to UI (#13)
    * feat: fixes related to UI
    
    * feat: fixed the image positions for menu
    
    * feat: fixed the biryani images
    
    ---------
    
    Co-authored-by: sreerajan <sreer@V-S_FAMILY>

--- src/components/FoodItemGrid.jsx
+++ src/components/FoodItemGrid.jsx
@@ -44,57 +44,63 @@ const FoodItemGrid = ({ items, localImages, cart, setCart }) => {
       {filteredItems.map((item, index) => {
         const quantity = getQuantity(item.id);
         return (
-          <div key={index} className="food-item-tile">
-            <h3 className="food-item-name">{item.name}</h3>
-            <div className="food-item-image-container">
-              <img
-                src={
-                  item.image?.startsWith("http")
-                    ? item.image
-                    : localImages[item.image] || VegBiryani
-                }
-                alt={item.name}
-                className="food-item-image"
-              />
-              {!item.isAvailable && (
-                <div className="sold-out-overlay">Sold Out</div>
-              )}
-              {quantity > 0 && <div className="quantity-badge">{quantity}</div>}
-            </div>
-            <div className="food-item-details">
-              <p className="food-item-description">{item.description}</p>
-              <div className="food-item-bottom">
-                <span className="food-item-price">{item.price} kr</span>
-                {item.isAvailable ? (
-                  quantity > 0 ? (
-                    <div className="quantity-controls">
-                      <button
-                        onClick={() => onRemoveFromCart(item)}
-                        disabled={quantity <= 1}
-                      >
-                        -
-                      </button>
-                      <span>{quantity}</span>
-                      <button
-                        onClick={() => onAddToCart(item)}
-                        disabled={quantity >= 10}
-                      >
-                        +
-                      </button>
-                    </div>
-                  ) : (
-                    <button
-                      className="cart-button"
-                      onClick={() => onAddToCart(item)}
-                    >
-                      Add to Cart
-                    </button>
-                  )
-                ) : (
-                  <button className="cart-button sold-out" disabled>
-                    Sold Out
-                  </button>
+          <div className="food-item-outer">
+            <div key={index} className="food-item-tile">
+              <div className="food-item-tite">
+                <h3 className="food-item-name">{item.name}</h3>
+              </div>
+              <div className="food-item-image-container">
+                <img
+                  src={
+                    item.image?.startsWith("http")
+                      ? item.image
+                      : localImages[item.image] || VegBiryani
+                  }
+                  alt={item.name}
+                  className="food-item-image"
+                />
+                {!item.isAvailable && (
+                  <div className="sold-out-overlay">Sold Out</div>
                 )}
+                {quantity > 0 && (
+                  <div className="quantity-badge">{quantity}</div>
+                )}
+              </div>
+              <div className="food-item-details">
+                <p className="food-item-description">{item.description}</p>
+                <div className="food-item-bottom">
+                  <span className="food-item-price">{item.price} kr</span>
+                  {item.isAvailable ? (
+                    quantity > 0 ? (
+                      <div className="quantity-controls">
+                        <button
+                          onClick={() => onRemoveFromCart(item)}
+                          disabled={quantity <= 1}
+                        >
+                          -
+                        </button>
+                        <span>{quantity}</span>
+                        <button
+                          onClick={() => onAddToCart(item)}
+                          disabled={quantity >= 10}
+                        >
+                          +
+                        </button>
+                      </div>
+                    ) : (
+                      <button
+                        className="cart-button"
+                        onClick={() => onAddToCart(item)}
+                      >
+                        Add to Cart
+                      </button>
+                    )
+                  ) : (
+                    <button className="cart-button sold-out" disabled>
+                      Sold Out
+                    </button>
+                  )}
+                </div>
               </div>
             </div>
           </div>
