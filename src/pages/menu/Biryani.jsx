73a6153a489cd5548f9eff2c87c70acaf4c50351
sreerajanp <33514791+sreerajanp@users.noreply.github.com>
2025-08-31 17:41:45 +0200

    Update Biryani.jsx

--- src/pages/menu/Biryani.jsx
+++ src/pages/menu/Biryani.jsx
@@ -2,20 +2,6 @@ import "./css/FoodMenu.css";
 import FoodItemGrid from "../../components/FoodItemGrid";
 import originalItems from "../../assets/configuration/biryaniItems.json";
 import { useEffect, useState } from "react";
-import BiryaniItems from "../../assets/configuration/biryaniItems.json";
-import ChickenBiryani from '../../assets/biryani/chicken.jpg';
-import HyderabadChicken from '../../assets/biryani/hyderabadChicken.jpg';
-import DindigulChicken from '../../assets/biryani/dindigulChicken.jpg';
-import LambBiryani from '../../assets/biryani/lamb.jpg';
-import HyderabadLamb from '../../assets/biryani/hyderabadLamb.jpg';
-import DindigulLamb from '../../assets/biryani/dindigulLamb.jpg';
-import FishBiryani from '../../assets/biryani/fish.jpg';
-import PrawnBiryani from '../../assets/biryani/prawn.jpg';
-import VegBiryani from '../../assets/biryani/vegetable.jpg';
-import PannerBiryani from '../../assets/biryani/panner.jpg';
-import MealMakerBiryani from '../../assets/biryani/mealMaker.jpg';
-import EggBiryani from '../../assets/biryani/eggBiryani.jpg';
-import MushroomBiryani from '../../assets/biryani/mushroomBiryani.jpg';
 
 // Local images
 import ChickenBiryani from "../../assets/biryani/chicken.jpg";
